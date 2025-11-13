e5d12a5615fbfff8afc32e8e5b345fc93183c691
atzolad <atzolad@gmail.com>
2025-11-12 17:03:49 -0800

    Starting to work on Roster

---
+++ roster/database.go
@@ -0,0 +1,113 @@
+package roster
+
+import (
+	"IFTP/db"
+	"database/sql"
+	"fmt"
+	"strings"
+)
+
+func RetrieveStudents(myDb *db.MyDatabase) ([]Student, error) {
+	rows, err := myDb.Db.Query(
+		"SELECT id, name, email, active FROM students WHERE active = true")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	// A students slice to hold the data from the returned rows
+	var students []Student
+
+	// Loop through rows, using Scan to assign column data to struct fields.
+	for rows.Next() {
+		var student Student
+		if err := rows.Scan(&student.ID, &student.Name, &student.Email, &student.Active); err != nil {
+			return nil, err
+		}
+		students = append(students, student)
+	}
+	if err = rows.Err(); err != nil {
+		return students, err
+	}
+	return students, nil
+}
+
+func InsertStudent(myDb *db.MyDatabase, s *Student) error {
+	err := myDb.Db.QueryRow(
+		"INSERT INTO students(name, email) VALUES($1, $2) RETURNING id",
+		s.Name, s.Email).Scan(&s.ID)
+
+	return err
+}
+
+func UpdateStudentDB(myDb *db.MyDatabase, id int, s *Student) (*Student, error) {
+	updates := []string{}
+	args := []any{}
+	argCount := 1
+
+	if s.Name != "" {
+		updates = append(updates, fmt.Sprintf("name=$%d", argCount))
+		args = append(args, s.Name)
+		argCount++
+	}
+
+	if s.Email != "" {
+		updates = append(updates, fmt.Sprintf("email=$%d", argCount))
+		args = append(args, s.Email)
+		argCount++
+	}
+
+	if len(updates) == 0 {
+		return nil, fmt.Errorf("no fields to update")
+	}
+
+	args = append(args, id)
+	// RETURNING gives you the updated row within one request to the DB
+	query := fmt.Sprintf("UPDATE students SET %s WHERE id=$%d AND active=true RETURNING id, name, email, active",
+		strings.Join(updates, ", "), argCount)
+
+	var updated Student
+	err := myDb.Db.QueryRow(query, args...).Scan(&updated.ID, &updated.Name, &updated.Email, &updated.Active)
+
+	if err == sql.ErrNoRows {
+		return nil, fmt.Errorf("active student with id %d not found", id)
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	return &updated, nil
+}
+
+func SoftDeleteStudentDB(myDb *db.MyDatabase, id int) (string, error) {
+
+	var name string
+
+	err := myDb.Db.QueryRow(
+		"SELECT name FROM students WHERE id=$1 AND active=true", id,
+	).Scan(&name)
+
+	if err == sql.ErrNoRows {
+		return "", fmt.Errorf("student with id %d not found", id)
+	}
+
+	result, err := myDb.Db.Exec(
+		"UPDATE students SET active = false WHERE id = $1",
+		id)
+
+	if err != nil {
+		return "", err
+	}
+
+	// Check if any row was actually updated
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return "", err
+	}
+
+	if rowsAffected == 0 {
+		return "", fmt.Errorf("student with id %d not found", id)
+	}
+
+	return name, nil
+}
---
+++ roster/handlers.go
@@ -0,0 +1,109 @@
+package roster
+
+import (
+	"IFTP/db"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Roster struct {
+	ID          int    `json:"id"`
+	Student_ID  string `json:"student_id"`
+	Class_ID    string `json:"class_id"`
+	Enrolled_At string `json:"enrolled_at"`
+	Active      bool   `json:"active"`
+}
+
+// GetStudents responds with the list of all users as JSON.
+func GetStudents(myDb *db.MyDatabase) gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		students, err := RetrieveStudents(myDb)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.Header("content-type", "application/json")
+		c.JSON(http.StatusOK, students)
+		fmt.Printf("Successfully retrieved student list \n")
+	}
+}
+
+// AddStudent adds an user from JSON received in the request body.
+func AddStudent(myDb *db.MyDatabase) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var newStudent Student
+
+		// Call BindJSON to bind the received JSON to
+		// newStudent.
+		if err := c.BindJSON(&newStudent); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		if err := InsertStudent(myDb, &newStudent); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		// // Add the new student to the slice.
+		// students = append(students, newStudent)
+		// c.Header("content-type", "application/json")
+		c.JSON(http.StatusCreated, newStudent)
+		fmt.Printf("Successfully created new student: %v", newStudent)
+	}
+}
+
+// Update Student updates the student details based on the JSON received in the request body.
+func UpdateStudent(myDb *db.MyDatabase) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		id := c.Param("id")
+		integerID, err := strconv.Atoi(id)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		}
+
+		var updateStudent Student
+
+		// Call BindJSON to bind the received JSON to updatedStudent
+		if err := c.BindJSON(&updateStudent); err != nil {
+			return
+		}
+
+		returnedStudent, err := UpdateStudentDB(myDb, integerID, &updateStudent)
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.Header("content-type", "application/json")
+		c.JSON(http.StatusOK, returnedStudent)
+		fmt.Printf("Successfully updated student: %v \n", returnedStudent.Name)
+	}
+}
+
+// SoftDeleteStudent changes the Active status of the student to false, rather than permanently deleting.
+func SoftDeleteStudent(myDb *db.MyDatabase) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		id := c.Param("id")
+
+		integerID, err := strconv.Atoi(id)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid student id"})
+		}
+
+		deletedStudent, err := SoftDeleteStudentDB(myDb, integerID)
+		if err != nil {
+			c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"message": fmt.Sprintf("student %v deleted successfully", deletedStudent)})
+		fmt.Printf("Successfully soft deleted student %v with id: %v \n", deletedStudent, integerID)
+	}
+}
