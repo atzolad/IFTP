b68abec27fda5bff7405a1e95ff68847b142a519
atzolad <atzolad@gmail.com>
2025-11-14 11:42:58 -0800

    Updated some of the prints to display the correct info (some class functions said student)

--- roster/handlers.go
+++ roster/handlers.go
@@ -4,7 +4,6 @@ import (
 	"IFTP/db"
 	"fmt"
 	"net/http"
-	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -17,93 +16,40 @@ type Roster struct {
 	Active      bool   `json:"active"`
 }
 
-// GetStudents responds with the list of all users as JSON.
-func GetStudents(myDb *db.MyDatabase) gin.HandlerFunc {
+// GetRoster responds with the overall enrolled class lists
+func GetRoster(myDb *db.MyDatabase) gin.HandlerFunc {
 	return func(c *gin.Context) {
 
-		students, err := RetrieveStudents(myDb)
+		fullRoster, err := GetRoster(myDb)
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 			return
 		}
 
 		c.Header("content-type", "application/json")
-		c.JSON(http.StatusOK, students)
-		fmt.Printf("Successfully retrieved student list \n")
+		c.JSON(http.StatusOK, fullRoster)
+		fmt.Printf("Successfully retrieved roster \n")
 	}
 }
 
-// AddStudent adds an user from JSON received in the request body.
-func AddStudent(myDb *db.MyDatabase) gin.HandlerFunc {
+// JoinClass adds the student to the class from the request.
+func JoinClass(myDb *db.MyDatabase) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		var newStudent Student
+		var newEnrollment Roster
 
 		// Call BindJSON to bind the received JSON to
 		// newStudent.
-		if err := c.BindJSON(&newStudent); err != nil {
+		if err := c.BindJSON(&newEnrollment); err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 			return
 		}
 
-		if err := InsertStudent(myDb, &newStudent); err != nil {
+		if err := JoinClass(myDb, &newEnrollment); err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 			return
 		}
 
-		// // Add the new student to the slice.
-		// students = append(students, newStudent)
-		// c.Header("content-type", "application/json")
-		c.JSON(http.StatusCreated, newStudent)
-		fmt.Printf("Successfully created new student: %v", newStudent)
-	}
-}
-
-// Update Student updates the student details based on the JSON received in the request body.
-func UpdateStudent(myDb *db.MyDatabase) gin.HandlerFunc {
-	return func(c *gin.Context) {
-		id := c.Param("id")
-		integerID, err := strconv.Atoi(id)
-		if err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		}
-
-		var updateStudent Student
-
-		// Call BindJSON to bind the received JSON to updatedStudent
-		if err := c.BindJSON(&updateStudent); err != nil {
-			return
-		}
-
-		returnedStudent, err := UpdateStudentDB(myDb, integerID, &updateStudent)
-
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-			return
-		}
-
-		c.Header("content-type", "application/json")
-		c.JSON(http.StatusOK, returnedStudent)
-		fmt.Printf("Successfully updated student: %v \n", returnedStudent.Name)
-	}
-}
-
-// SoftDeleteStudent changes the Active status of the student to false, rather than permanently deleting.
-func SoftDeleteStudent(myDb *db.MyDatabase) gin.HandlerFunc {
-	return func(c *gin.Context) {
-		id := c.Param("id")
-
-		integerID, err := strconv.Atoi(id)
-		if err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid student id"})
-		}
-
-		deletedStudent, err := SoftDeleteStudentDB(myDb, integerID)
-		if err != nil {
-			c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
-			return
-		}
-
-		c.JSON(http.StatusOK, gin.H{"message": fmt.Sprintf("student %v deleted successfully", deletedStudent)})
-		fmt.Printf("Successfully soft deleted student %v with id: %v \n", deletedStudent, integerID)
+		c.JSON(http.StatusCreated, newEnrollment)
+		fmt.Printf("Successfully created new student: %v", newEnrollment)
 	}
 }
