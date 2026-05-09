d721b005fe107517b87236fce57c1d9abd72ae77
atzolad <atzolad@gmail.com>
2026-05-08 21:03:09 -0700

    Removed search button from main top bar and grouped the list classes by month by name

--- class/database.go
+++ class/database.go
@@ -71,7 +71,7 @@ func dbListClassesByMonth(ctx context.Context, myDb *db.MyDatabase, month string
 	}
 
 	query.WriteString(" GROUP BY cs.month, c.id, c.name, c.teacher, c.day_of_week, c.time, c.description, c.capacity")
-	query.WriteString(" ORDER BY cs.month DESC")
+	query.WriteString(" ORDER BY c.name, cs.month DESC")
 
 	rows, err := myDb.Pool.Query(ctx, query.String(), args...)
 
